1c53590244a32f81e949802e04fabcc03505bd96
nikilpillai2002 <nsatheeshpillai922@student.fuhsd.org>
2017-10-02 15:53:17 -0700

    This is the vuforia code that should work givent that the license key is valuide

--- TeamCode/src/main/java/org/firstinspires/ftc/teamcode/BillsAuto.java
+++ TeamCode/src/main/java/org/firstinspires/ftc/teamcode/BillsAuto.java
@@ -30,12 +30,69 @@
 package org.firstinspires.ftc.teamcode;
 
 import com.qualcomm.robotcore.eventloop.opmode.Autonomous;
+import com.qualcomm.robotcore.eventloop.opmode.LinearOpMode;
+import com.qualcomm.robotcore.hardware.DcMotor;
+import com.qualcomm.robotcore.util.ElapsedTime;
 
 /**
  * Created by beta on 10/2/17.
  */
 @Autonomous(name = "Autonomousv1.0", group = "Team-A")
-public class BillsAuto {
+public class BillsAuto extends LinearOpMode{
 
+    org.firstinspires.ftc.robotcontroller.external.samples.HardwarePushbot robot   = new org.firstinspires.ftc.robotcontroller.external.samples.HardwarePushbot();
+    private ElapsedTime     runtime = new ElapsedTime();
 
+    static final double     COUNTS_PER_MOTOR_REV    = 1120 ;    // eg: TETRIX Motor Encoder
+    static final double     DRIVE_GEAR_REDUCTION    = 1.0 ;     // This is < 1.0 if geared UP
+    static final double     WHEEL_DIAMETER_INCHES   = 4.0 ;     // For figuring circumference
+    static final double     COUNTS_PER_INCH         = (COUNTS_PER_MOTOR_REV * DRIVE_GEAR_REDUCTION) /
+            (WHEEL_DIAMETER_INCHES * 3.1415);
+    static final double     DRIVE_SPEED             = 0.3;
+    static final double     TURN_SPEED              = 0.2;
+    static final double     ROBOT_SPEED             = 2.25; // ft/sec
+    static final double     ROBOT_LENGTH            = 1.25;
+    public void encoderDrive(double speed, double leftInches, double rightInches, double timeoutS) throws InterruptedException {
+        int newLeftTarget;
+        int newRightTarget;
+        // Ensure that the opmode is still active
+        if (opModeIsActive()) {
+            // Determine new target position, and pass to motor controller
+            newLeftTarget = robot.leftMotor.getCurrentPosition() + (int) (leftInches * COUNTS_PER_INCH);
+            newRightTarget = robot.rightMotor.getCurrentPosition() + (int) (rightInches * COUNTS_PER_INCH);
+            robot.leftMotor.setTargetPosition(newLeftTarget);
+            robot.rightMotor.setTargetPosition(newRightTarget);
+
+            // Turn On RUN_TO_POSITION
+            robot.leftMotor.setMode(DcMotor.RunMode.RUN_TO_POSITION);
+            robot.rightMotor.setMode(DcMotor.RunMode.RUN_TO_POSITION);
+
+            // reset the timeout time and start motion.
+            runtime.reset();
+            robot.leftMotor.setPower(Math.abs(speed) * 0.898);
+            robot.rightMotor.setPower(Math.abs(speed));
+
+            // keep looping while we are still active, and there is time left, and both motors are running.
+            while (opModeIsActive() &&
+                    (runtime.seconds() < timeoutS) &&
+                    (robot.leftMotor.isBusy() && robot.rightMotor.isBusy())) {
+                // Display it for the driver.
+                telemetry.addData("Path1", "Running to %7d :%7d", newLeftTarget, newRightTarget);
+                telemetry.addData("Path2", "Running at %7d :%7d",
+                        robot.leftMotor.getCurrentPosition(),
+                        robot.rightMotor.getCurrentPosition());
+                telemetry.update();
+
+                // Allow time for other processes to run.
+                idle();
+            }
+
+            // Stop all motion;
+            robot.leftMotor.setPower(0);
+            robot.rightMotor.setPower(0);
+
+            // Turn off RUN_TO_POSITION
+
+        }
+    }
 }
