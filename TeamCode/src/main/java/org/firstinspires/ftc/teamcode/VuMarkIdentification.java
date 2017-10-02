b33f8778ec47549a11ca1550e57d5a8fdcc289c6
Willexgm <wstuff@mincher.com>
2017-10-02 15:57:15 -0700

    Fix

--- TeamCode/src/main/java/org/firstinspires/ftc/teamcode/VuMarkIdentification.java
+++ TeamCode/src/main/java/org/firstinspires/ftc/teamcode/VuMarkIdentification.java
@@ -94,7 +94,7 @@ public class VuMarkIdentification extends LinearOpMode {
         // VuforiaLocalizer.Parameters parameters = new VuforiaLocalizer.Parameters();
 
         /*
-         * IMPORTANT: You need to obtain your own license key to use Vuforia. The string below with which
+         * IMPORTANT: You need to obtain your own license key to use Vuforia.The string below with which
          * 'parameters.vuforiaLicenseKey' is initialized is for illustration only, and will not function.
          * A Vuforia 'Development' license key, can be obtained free of charge from the Vuforia developer
          * web site at https://developer.vuforia.com/license-manager.
